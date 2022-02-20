3f36526e4788bf367e3f0ed01f4f92058cb738df
aditya1343 <99969931+aditya1343@users.noreply.github.com>
2022-02-20 18:38:34 +0530

    Update access_elements_using_index.py

--- access_elements_using_index.py
+++ access_elements_using_index.py
@@ -9,4 +9,5 @@ print("\n Last element of the array is: ",names[-1])
 #printing 3rd element of the 2nd row
 
 numbers = np.array([[5,8,3],[3,1,2],[5,7,8]])
+
 print("Second Row, third element:",numbers[1][2])
